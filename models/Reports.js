4cf1fcb9332ba5baba75f6106289662de702c7f8
marcusprice <marcusprice88@gmail.com>
2019-04-26 21:36:15 -0700

    started unit testing on reports model

---
+++ models/Reports.js
@@ -0,0 +1,91 @@
+const pg = require('./pg');
+const validation = require('../lib/validation');
+
+module.exports = {
+  createReport: (reportData, callback) => {
+    //validate input data
+    const possibleKeys = ['userID', 'name', 'description', 'subreddits', 'filteredIn', 'filteredOut', 'submissionLimit', 'notifications'];
+    const validationError = validation.validateInputData(reportData, possibleKeys);
+
+    if(validationError) {
+      callback(validationError, null);
+    } else {
+      const sql = 'INSERT INTO Reports ' +
+      '(UserID, Name, Description, Subreddits, FilteredIn, FilteredOut, SubmissionLimit, DateCreated, LastEdit, Notifications)' +
+      'VALUES ($1, $2, $3, $4, $5, $6, $7, now(), now(), $8);';
+
+      const values = [
+        reportData.userID,
+        reportData.name,
+        reportData.description,
+        reportData.subreddits,
+        reportData.filteredIn,
+        reportData.filteredOut,
+        reportData.submissionLimit,
+        reportData.notifications
+      ];
+
+      pg.query(sql, values, (err, result) => {
+        if(err) {
+          callback(err, null);
+        } else {
+          callback(null, true);
+        }
+      });
+    }
+  },
+
+  readReport: (reportID, callback) => {
+    if(typeof(reportID) !== 'number') {
+      const error = new Error('the input was not a number');
+      callback(error, null);
+    } else {
+      const sql = 'SELECT * FROM Reports WHERE ReportID = $1';
+      const values = [reportID];
+
+      pg.query(sql, values, (err, result) => {
+        if(err) {
+          callback(err, null);
+        } else {
+          if(result.rows.length < 1) {
+            const error = new Error('no results found');
+            callback(error, null);
+          } else {
+            callback(null, result.rows[0]);
+          }
+        }
+      });
+    }
+  },
+
+  readAllReportsByUser: (userID, callback) => {
+    if(typeof(userID) !== 'number') {
+      const error = new Error('the input was not a number');
+      callback(error, null);
+    } else {
+      const sql = 'SELECT * FROM Reports WHERE UserID = $1';
+      const values = [userID];
+
+      pg.query(sql, values, (err, result) => {
+        if(err) {
+          callback(err, null);
+        } else {
+          if(result.rows.length < 1) {
+            const error = new Error('no results found');
+            callback(error, null);
+          } else {
+            callback(null, result.rows);
+          }
+        }
+      });
+    }
+  },
+
+  updateReport: () => {
+
+  },
+
+  deleteReport: () => {
+
+  }
+}
--- test/models/ReportsTest.js
+++ test/models/ReportsTest.js
@@ -3,18 +3,134 @@ const expect = require('chai').expect;
 
 describe('Reports', () => {
 
+  const newValidReportData = {
+    userID: 1,
+    name: 'My Ask Reddit Report',
+    description: 'Gets the best ask-reddit threads',
+    subreddits: 'askreddit',
+    filteredIn: '',
+    filteredOut: '',
+    submissionLimit: 60,
+    notifications: true
+  };
+
+  const reportDataExtraField = {
+    userID: 1,
+    name: 'My Ask Reddit Report',
+    description: 'Gets the best ask-reddit threads',
+    subreddits: 'askreddit',
+    filteredIn: '',
+    filteredOut: '',
+    submissionLimit: 60,
+    notifications: true,
+    extraField: ''
+  };
+
+  const reportDataMissingField = {
+    userID: 1,
+    name: 'My Ask Reddit Report',
+    description: 'Gets the best ask-reddit threads',
+    subreddits: 'askreddit',
+    filteredIn: '',
+    filteredOut: '',
+    submissionLimit: 60
+  };
+
+  const reportDataInvalidField = {
+    userID: 1,
+    name: 'My Ask Reddit Report',
+    description: 'Gets the best ask-reddit threads',
+    subreddits: 'askreddit',
+    invalidField: '',
+    filteredOut: '',
+    submissionLimit: 60
+  };
+
   describe('Reports.createReport()', () => {
-    it('should return true upon successful insert');
-    it('should return an error if the input has too many fields');
-    it('should return an error if the unput has too little fields');
-    it('should return an error if the input has an invalid field');
+    it('should return true upon successful insert', (done) => {
+      Reports.createReport(newValidReportData, (error, result) => {
+        expect(error).to.be.a('null');
+        expect(result).to.be.true;
+        done();
+      });
+    });
+
+    it('should return an error if the input has too many fields', (done) => {
+      Reports.createReport(reportDataExtraField, (error, result) => {
+        expect(error).to.be.an('error');
+        expect(result).to.be.a('null');
+        done();
+      });
+    });
+
+    it('should return an error if the unput has too little fields', (done) => {
+      Reports.createReport(reportDataMissingField, (error, result) => {
+        expect(error).to.be.an('error');
+        expect(result).to.be.a('null');
+        done();
+      });
+    });
+
+    it('should return an error if the input has an invalid field', (done) => {
+      Reports.createReport(reportDataInvalidField, (error, result) => {
+        expect(error).to.be.an('error');
+        expect(result).to.be.a('null');
+        done();
+      });
+    });
   });
 
 
   describe('Reports.readReport()', () => {
-    it('should return an object with input of a ReportID');
-    it('should return an error if no rows are found');
-    it('should return an error if the input is not a number or string');
+    it('should return an object with input of a ReportID', (done) => {
+      Reports.readReport(1, (error, result) => {
+        expect(result).to.be.an('object');
+        expect(error).to.be.a('null');
+        done();
+      });
+    });
+
+    it('should return an error if no rows are found', (done) => {
+      Reports.readReport(1000000, (error, result) => {
+        expect(error).to.be.an('error');
+        expect(result).to.be.a('null');
+        done();
+      });
+    });
+
+    it('should return an error if the input is not a number or string', (done) => {
+      Reports.readReport({}, (error, result) => {
+        expect(error).to.be.an('error');
+        expect(result).to.be.a('null');
+        done();
+      });
+    });
+  });
+
+  describe('Reports.readAllReportsByUser()', () => {
+    it('should return an array of objects with input of a UserID', (done) => {
+      Reports.readAllReportsByUser(1, (error, result) => {
+        expect(result).to.be.an('array');
+        expect(error).to.be.a('null');
+        done();
+      });
+    });
+
+    it('should return an error if no rows are found', (done) => {
+      Reports.readAllReportsByUser(1000000, (error, result) => {
+        expect(error).to.be.an('error');
+        expect(result).to.be.a('null');
+        done();
+      });
+    });
+
+    it('should return an error if the input is not a number or string', (done) => {
+      Reports.readAllReportsByUser({}, (error, result) => {
+        expect(error).to.be.an('error');
+        expect(result).to.be.a('null');
+        done();
+      });
+    });
   });
 
 
