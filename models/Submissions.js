83d9d741ea4d45ad37e3e9041d3ea0e1eac38e06
marcusprice <marcusprice88@gmail.com>
2019-04-27 16:28:20 -0700

    all submission unit tests are passing

--- models/Submissions.js
+++ models/Submissions.js
@@ -37,15 +37,85 @@ module.exports = {
     }
   },
 
-  readSubmission: () => {
+  readSubmission: (submissionID, callback) => {
+    if(typeof(submissionID) !== 'number') {
+      const error = new Error('input was not a number');
+      callback(error, null);
+    } else {
+      const sql = 'SELECT * FROM Submissions WHERE SubmissionID = $1';
+      const values = [submissionID];
 
+      pg.query(sql, values, (err, result) => {
+        if(err) {
+          callback(err, null);
+        } else {
+          if(result.rows.length < 1) {
+            const error = new Error('no results found');
+            callback(error, null);
+          } else {
+            callback(null, result.rows[0]);
+          }
+        }
+      });
+    }
   },
 
-  updateSubmission: () => {
+  updateSubmission: (submissionID, submissionData, callback) => {
+    //validate input data
+    const possibleKeys = [
+      'title',
+      'url',
+      'posterHandle',
+      'upvotes',
+      'downvotes'
+    ];
+    const validationError = validation.validateInputData(submissionData, possibleKeys);
 
+    if(validationError) {
+      callback(validationError, null);
+    } else {
+      const sql = 'UPDATE Submissions ' +
+      'SET Title = $1, URL = $2, PosterHandle = $3, Upvotes = $4, Downvotes = $5 ' +
+      'WHERE SubmissionID = $6;';
+
+      const values = [
+        submissionData.title,
+        submissionData.url,
+        submissionData.posterHandle,
+        submissionData.upvotes,
+        submissionData.downvotes,
+        submissionID
+      ];
+
+      pg.query(sql, values, (err, result) => {
+        if(err) {
+          callback(err, null);
+        } else {
+          callback(null, true);
+        }
+      });
+    }
   },
 
-  deleteSubmission: () => {
+  deleteSubmission: (submissionID, callback) => {
+    if(typeof(submissionID) !== 'number') {
+      const error = new Error('the input was not a number');
+      callback(error, null);
+    } else {
+      const sql = 'DELETE FROM Submissions WHERE SubmissionID = $1;';
+      const values = [submissionID];
 
+      pg.query(sql, values, (err, result) => {
+        if(err) {
+          callback(err, null);
+        } else {
+          if(result.rows.length < 1) {
+            callback(null, true);
+          } else {
+            callback(null, true);    
+          }
+        }
+      });
+    }
   }
 }
--- test/models/SubmissionsTest.js
+++ test/models/SubmissionsTest.js
@@ -11,7 +11,7 @@ describe('Submissions', () => {
     downvotes: 5
   };
 
-  const validSubmissionDataExtraField = {
+  const submissionDataExtraField = {
     title: 'Submission Title',
     url: 'https://www.google.com',
     posterHandle: 'endlesshappiness',
@@ -20,7 +20,23 @@ describe('Submissions', () => {
     extraField: ''
   };
 
+  const submissionDataMissingField = {
+    title: 'Submission Title',
+    url: 'https://www.google.com',
+    posterHandle: 'endlesshappiness',
+    upvotes: 20,
+  };
+
+  const submissionDatainvalidField = {
+    title: 'Submission Title',
+    url: 'https://www.google.com',
+    posterHandle: 'endlesshappiness',
+    upvotes: 20,
+    invalidField: ''
+  };
+
   describe('Submissions.createSubmission()', () => {
+
     it('should return true upon successful insert', (done) => {
       Submissions.createSubmission(validSubmissionData, (error, result) => {
         expect(error).to.be.a('null');
@@ -28,35 +44,110 @@ describe('Submissions', () => {
         done();
       });
     });
+
     it('should return an error if the input has too many fields', (done) => {
-      Submissions.createSubmission(validSubmissionDataExtraField, (error, result) => {
+      Submissions.createSubmission(submissionDataExtraField, (error, result) => {
         expect(error).to.be.an('error');
         expect(result).to.be.a('null');
         done();
       });
     });
-    it('should return an error if the unput has too little fields');
-    it('should return an error if the input has an invalid field');
+
+    it('should return an error if the input has too little fields', (done) => {
+      Submissions.createSubmission(submissionDataMissingField, (error, result) => {
+        expect(error).to.be.an('error');
+        expect(result).to.be.a('null');
+        done();
+      });
+    });
+
+    it('should return an error if the input has an invalid field', (done) => {
+      Submissions.createSubmission(submissionDatainvalidField, (error, result) => {
+        expect(error).to.be.an('error');
+        expect(result).to.be.a('null');
+        done();
+      });
+    });
   });
 
   describe('Submissions.readSubmission()', () => {
-    it('should return an object with input of a ReportID');
-    it('should return an error if no rows are found');
-    it('should return an error if the input is not a number or string');
+    it('should return an object with input of a ReportID', (done) => {
+      Submissions.readSubmission(1, (error, result) => {
+        expect(error).to.be.a('null');
+        expect(result).to.be.an('object');
+        done();
+      });
+    });
+
+    it('should return an error if no rows are found', (done) => {
+      Submissions.readSubmission(10000, (error, result) => {
+        expect(error).to.be.an('error');
+        expect(result).to.be.a('null');
+        done();
+      });
+    });
+
+    it('should return an error if the input is not a number or string', (done) => {
+      Submissions.readSubmission({}, (error, result) => {
+        expect(error).to.be.an('error');
+        expect(result).to.be.a('null');
+        done();
+      });
+    });
   });
 
 
   describe('Submissions.updateSubmission()', () => {
-    it('should return true upon successful update');
-    it('should return an error if the input has too many fields');
-    it('should return an error if the unput has too little fields');
-    it('should return an error if the input has an invalid field');
+    it('should return true upon successful update', (done) => {
+      Submissions.updateSubmission(1, validSubmissionData, (error, result) => {
+        expect(error).to.be.a('null');
+        expect(result).to.be.true;
+        done();
+      });
+    });
+
+    it('should return an error if the input has too many fields', (done) => {
+      Submissions.updateSubmission(1, submissionDataExtraField, (error, result) => {
+        expect(error).to.be.an('error');
+        expect(result).to.be.a('null');
+        done();
+      });
+    });
+
+    it('should return an error if the unput has too little fields', (done) => {
+      Submissions.updateSubmission(1, submissionDataMissingField, (error, result) => {
+        expect(error).to.be.an('error');
+        expect(result).to.be.a('null');
+        done();
+      });
+    });
+
+    it('should return an error if the input has an invalid field', (done) => {
+      Submissions.updateSubmission(1, submissionDatainvalidField, (error, result) => {
+        expect(error).to.be.an('error');
+        expect(result).to.be.a('null');
+        done();
+      });
+    });
   });
 
 
   describe('Submissions.deleteSubmission()', () => {
-    it('should return true upon successful update');
-    it('should return an error if the input is not a number or string');
+    it('should return true upon successful delete', (done) => {
+      Submissions.deleteSubmission(2, (error, result) => {
+        expect(error).to.be.a('null');
+        expect(result).to.be.true;
+        done();
+      });
+    });
+
+    it('should return an error if the input is not a number', (done) => {
+      Submissions.deleteSubmission({}, (error, result) => {
+        expect(error).to.be.an('error');
+        expect(result).to.be.a('null');
+        done();
+      });
+    });
   });
 
 });
