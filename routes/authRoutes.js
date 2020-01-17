a827466a89955337d365804e3294d5753a883ecd
Price <marcusprice88@gmail.com>
2020-01-17 12:47:36 -0800

    started on documentation for auth routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,22 +1,36 @@
-const users = require('../controllers/users');
-const helpers = require('../lib/helpers');
+/**
+ * Authorization Routes
+ * @module authRoutes
+ */
 
+const users = require('../controllers/users');  //users controller
+const helpers = require('../lib/helpers');      //helper library
+
+/**
+ * Handles user authorization for basic login, logout and remember me tasks
+ * @param {object} app - instance of express
+ */
 module.exports = (app) => {
+
+  /**
+   * Check to see if the user is logged in and has rememberme set
+   * @param {object} req - the express request object
+   * @param {object} res - the express response object
+   * @return {string/json} - data about the user
+   */
   app.get('/checkLoginStatus', async (req, res) => {
-    let output = {};
-    if(req.session.loggedIn && req.session.rememberMe) {
-      output.loggedIn = true;
-      //get user data
+    //create output variable with login initialized to false
+    let output = { loggedIn: false };
+    if(req.session.loggedIn && req.session.rememberMe) {  //user is logged in and has remember me set
+      output.loggedIn = true; //user us logged in
+
+      //add user data & report data to output object
       output.userData = await users.getUser(req.session.userID);
       output.reportData = await helpers.getAllReportData(req.session.userID);
-      res.json(output);
-    } else {
-      //output.loggedIn = false;
-      output.loggedIn = true;
-      output.userData = await users.getUser(3);
-      output.reportData = await helpers.getAllReportData(3);
-      res.json(output);
     }
+
+    //convert output object to json and send to client
+    res.json(output);
   });
 
   app.get('/login', async (req, res) => {
